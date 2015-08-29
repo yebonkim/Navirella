d192df3a317db8df896029075315254fcb14287c
JiyoungPark <realplay23@naver.com>
2015-08-29 17:54:44 +0900

    NavirellaActivity add

--- AndroidApp/app/src/main/java/com/withcamp/soma6/navirella/MainActivity.java
+++ AndroidApp/app/src/main/java/com/withcamp/soma6/navirella/MainActivity.java
@@ -1,9 +1,10 @@
 package com.withcamp.soma6.navirella;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,29 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Button btn_mainstart = (Button) findViewById(R.id.btn_mainstart);
 
-        
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        btn_mainstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NavirellaActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
---
+++ AndroidApp/app/src/main/java/com/withcamp/soma6/navirella/NavirellaActivity.java
@@ -0,0 +1,14 @@
+package com.withcamp.soma6.navirella;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NavirellaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navirella);
+
+    }
+}
