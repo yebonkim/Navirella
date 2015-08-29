f3a67455fd662a53a4557fefebe0f7a0f4ee5534
JiyoungPark <realplay23@naver.com>
2015-08-29 21:14:24 +0900

    GPS 기능 추가

--- AndroidApp/app/src/main/java/com/withcamp/soma6/navirella/NavirellaActivity.java
+++ AndroidApp/app/src/main/java/com/withcamp/soma6/navirella/NavirellaActivity.java
@@ -2,6 +2,9 @@ package com.withcamp.soma6.navirella;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class NavirellaActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class NavirellaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navirella);
 
+        final Button btn_findstart = (Button) findViewById(R.id.btn_findstart);
+        final TextView textview_startpoint = (TextView) findViewById(R.id.textview_startpoint);
+        final GpsInfo gps = new GpsInfo(NavirellaActivity.this);
+
+        btn_findstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double latitude = gps.getLatitude();
+                double longitude = gps.getLongitude();
+                textview_startpoint.setText(String.valueOf(latitude) + " / " + String.valueOf(longitude));
+            }
+        });
     }
 }
